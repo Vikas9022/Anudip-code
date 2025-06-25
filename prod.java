d4e98ccb5efa75a0804df19e32380f3cd23371e5
Vikas9022 <130819784+Vikas9022@users.noreply.github.com>
2025-06-25 13:36:52 +0530

    Add files via upload

---
+++ prod.java
@@ -0,0 +1,19 @@
+
+    
+class prod {
+    int ok(){
+    int a = 10;
+    int b = 50;
+    int sum = a*b;
+    return sum;
+    }
+
+    public static void main(String[] args) {
+        
+        prod h1 = new prod();
+        int sum = h1.ok ();
+        System.out.println(sum);
+}
+}
+
+    
