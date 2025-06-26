dc0afc73c45a2b4bfd5dc5adc62835832de5acb4
Vikas Shinde <130819784+Vikas9022@users.noreply.github.com>
2025-06-26 11:57:14 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,16 @@
+public class pattern6 {
+    public static void main(String[] args) {
+        int m = 5;
+        int i,j;
+        int num = 1;
+        for(i = 0; i <= m; i++) {
+            System.out.println("");
+            for(j = 0; j <= i; j++){
+                System.out.print(num+" ");
+                num++;
+           }
+        }
+    }
+}
+    
+
